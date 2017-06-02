f977719f258f4e117be7821e3ed70e7c99468f6e
Kevin Martensson <kevinmartensson@gmail.com>
2017-06-02 14:45:34 +0200

    Add test for deep sorted arrays

--- test.js
+++ test.js
@@ -33,19 +33,28 @@ test('deep option', t => {
 
 	const obj1 = {b: 0};
 	const obj2 = {d: 0};
+	const obj3 = {a: [{b: 0}]};
+	const obj4 = {a: [{d: 0}]};
 
 	obj1.a = obj2;
 	obj2.c = obj1;
+	obj3.a[0].a = obj4.a[0];
+	obj4.a[0].c = obj3.a[0];
 
 	t.notThrows(() => {
 		m(obj1, {deep: true});
 		m(obj2, {deep: true});
+		m(obj3, {deep: true});
+		m(obj4, {deep: true});
 	});
 
 	const sorted = m(obj1, {deep: true});
+	const deepSorted = m(obj3, {deep: true});
 
 	t.is(sorted, sorted.a.c);
+	t.deepEqual(deepSorted.a[0], deepSorted.a[0].a.c);
 	t.deepEqual(Object.keys(sorted), ['a', 'b']);
+	t.deepEqual(Object.keys(deepSorted.a[0]), ['a', 'b']);
 	t.deepEqual(m({c: {c: 0, a: 0, b: 0}, a: 0, b: 0, z: [9, 8, 7, 6, 5]}, {deep: true}), {a: 0, b: 0, c: {a: 0, b: 0, c: 0}, z: [9, 8, 7, 6, 5]});
 	t.deepEqual(Object.keys(m({a: [{b: 0, a: 0}]}, {deep: true}).a[0]), ['a', 'b']);
 });
